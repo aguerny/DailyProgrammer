ca6c23c4a2ed2cb0601bb3014168111e5885b360
Allison Guerny <allison.guerny@gmail.com>
2013-12-10 16:14:40 -0500

    Part 1

---
+++ Easy001.js
@@ -0,0 +1,13 @@
+/*Daily Programmer Easy #1
+
+create a program that will ask the users name, age, and reddit username. have it tell them the information back, in the format:
+
+your name is (blank), you are (blank) years old, and your username is (blank)
+
+for extra credit, have the program log this information in a file to be accessed later.*/
+
+
+var name = prompt("What is your name?");
+var age = prompt("What is your age");
+var username = prompt("What is your reddit username?");
+alert("Your name is " + name + ", you are " + age + " years old, and your reddit username is " + username + ".");
