c55e23d675238c41092566872acaef977ca9fd26
Allison Guerny <allison.guerny@gmail.com>
2014-01-03 14:22:19 -0500

    Updated description of Easy145

--- Easy145.js
+++ Easy145.js
@@ -1,3 +1,6 @@
+/* Easy145
+Your goal is to draw a tree given the base-width of the tree (the number of characters on the bottom-most row of the triangle section). This "tree" must be drawn through ASCII art-style graphics[2] on standard console output. It will consist of a 1x3 trunk on the bottom, and a triangle shape on the top. The tree must be centered, with the leaves growing from a base of N-characters, up to a top-layer of 1 character. Each layer reduces by 2 character, so the bottom might be 7, while shrinks to 5, 3, and 1 on top layers.*/
+
 function tree(base, trunk, leaves) {
     var space = (base-1)/2;
     for (var width=1; width <= base; width += 2) {
